464e1c28cae3ccf0df69e9b4e9be6ec2c052179a
paliwalankita <paliwalankita31@gmail.com>
2021-07-06 17:15:20 +0530

    eight community page ui

---
+++ app/src/main/java/com/example/eight/Activity/StationsLikeAdapter.java
@@ -0,0 +1,104 @@
+package com.example.eight.Activity;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.eight.R;
+
+import java.util.ArrayList;
+
+public class StationsLikeAdapter extends RecyclerView.Adapter<StationsLikeAdapter.MyViewHolder> {
+
+    private Context context;
+    private ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView users_listen_count, users_follow_count, mj_rating, station_name, mj_name ;
+        ImageView stationImage, mj_image;
+        LinearLayout parentLayout;
+        RelativeLayout stationParentLayout;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+//            module_image = itemView.findViewById(R.id.module_image);
+//            module_name = itemView.findViewById(R.id.module_name);
+//            parent_layout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+
+//    public StationsAdapter(List<Modules> modulesList, Context context) {
+//        this.modulesList = modulesList;
+//        this.context = context;
+//    }
+
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+//        height = viewGroup.getMeasuredHeight();
+//        width = viewGroup.getMeasuredWidth();
+        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.stations_like_adapter, viewGroup, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder myViewHolder, int i) {
+//        final Modules modules = modulesList.get(i);
+        final int position = i;
+        /*Glide.with(context)
+                .load(modules.getModule_image())
+                .placeholder(R.drawable.channels_default_image)
+                .transition(withCrossFade())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(myViewHolder.module_image);*/
+
+//        myViewHolder.module_name.setText(modules.getModule_name());
+        /*myViewHolder.parent_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(context, ModuleDocumentShowcaseActivity.class);
+//                intent.putExtra(ResponseParameter.MDOC_MID, modules.getMid());
+//                intent.putExtra(ResponseParameter.MDOC_NAME, modules.getModule_name());
+//                intent.putExtra(ResponseParameter.MDOC_DESC, modules.getMod_desc());
+//                intent.putExtra(ResponseParameter.MDOC_CREATED_ON, modules.getMod_created_on());
+//                context.startActivity(intent);
+            }
+        });*/
+
+
+    /*myViewHolder.modulePartialView.post(new Runnable() {
+      @Override
+      public void run() {
+        if (height == 0) {
+          width = myViewHolder.moduleFullView.getMeasuredWidth();
+          height = myViewHolder.moduleFullView.getMeasuredHeight();
+        }
+        int reqWidth = width * Integer.parseInt(modules.getPercent_completed())/100;
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(reqWidth,height);
+        myViewHolder.modulePartialView.setLayoutParams(params);
+      }
+    });
+
+    if(position == modulesList.size()-1){
+      myViewHolder.outlineView.setVisibility(View.GONE);
+    }*/
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 1;
+    }
+}
+
---
+++ app/src/main/java/com/example/eight/CommunityMainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.eight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CommunityMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_community_main)
+    }
+}
